c08e7ca1262e16c2b40e46db1c1770fa36e3b6f3
AmogusFan113 <IDraw4rt@gmail.com>
2025-01-26 23:50:34 +0000

    Updated player movement, added coyote time, added custom input actions for left right and jump

--- PlayerCharacter/player_character.gd
+++ PlayerCharacter/player_character.gd
@@ -6,15 +6,19 @@ class_name Player
 
 #Physics Related Attributes
 const SPEED = 400.0
-const DASH_SPEED = 1000.0
+const DASH_SPEED = 1500.0
 const JUMP_VELOCITY = -500.0
+const ACCELERATION = 3500
+const DECELERATION = 5000
+const AIR_DECEL = 1000
 
 var double_jump: bool = false 
-var dash: bool = true #determines whether the player can dash or not
+var can_dash: bool = true #determines whether the player can dash or not
 var last_direction: int #last_direction tells you which direction the player was in last.
 var lock_dash: bool = false #locks the velocity when dashing.
 var has_control:bool = true #bool which determines whether the player has control over the character 
 var is_in_place: bool = false #if the player is NOT in control and is IN place, then it will not change its x or y dir.
+var coyote_jump: bool = false
 
 #Player Character Attributes (HP,MP)
 
@@ -22,40 +26,46 @@ func _ready():
 	pass
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if has_control:
 		if not is_on_floor():
 			velocity += get_gravity() * delta
+			if coyote_jump and $CoyoteTimer.is_stopped():
+				$CoyoteTimer.start(0.3)
 		else:
+			$CoyoteTimer.stop()
 			double_jump = true
+			coyote_jump = true
 
 		# Handle jump and double jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-		elif Input.is_action_just_pressed("ui_accept") and double_jump:
-			velocity.y = JUMP_VELOCITY
-			double_jump = false
+		if Input.is_action_just_pressed("Jump"):
+			if is_on_floor():
+				velocity.y = JUMP_VELOCITY
+			elif coyote_jump:
+				velocity.y = JUMP_VELOCITY
+				coyote_jump = false
+			elif double_jump:
+				velocity.y = JUMP_VELOCITY
+				double_jump = false
 
 		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction := Input.get_axis("ui_left", "ui_right")
+		var direction := Input.get_axis("Left", "Right")
 		if direction:
-			velocity.x = direction * SPEED
+			velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta)
 			last_direction = direction
+		elif is_on_floor():
+			velocity.x = move_toward(velocity.x, 0, DECELERATION * delta)
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.x = move_toward(velocity.x, 0, AIR_DECEL * delta)
 		
 		# handles dash.
-		if Input.is_action_just_pressed("Dash") and dash:
-			dash = false
+		if Input.is_action_just_pressed("Dash") and can_dash:
+			can_dash = false
 			lock_dash = true 
 			has_control = false
 			$DashCooldown.start(1) #adds a 1 second cooldown to dash 
 			$ControlCooldown.start(0.1)
 			$DashTimer.start(0.1) #makes the dash happen for 0.1 seconds 
 			print(get_last_motion())
-		
-			pass
 	
 	else:	
 		if lock_dash:
@@ -71,16 +81,15 @@ func set_y_velocity(y):
 	
 #when timer finishes, dash can happen again.
 func _on_dash_timer_timeout() -> void:
+	velocity.x = sign(velocity.x) * SPEED
 	lock_dash = false
-	pass # Replace with function body.
 
 func _on_dashcooldown_timeout() -> void:
-	dash = true
-	pass # Replace with function body.
-
+	can_dash = true
 
 func _on_control_cooldown_timeout() -> void:
 	has_control = true
 	is_in_place = false
-	pass # Replace with function body.
  
+func _on_coyote_timer_timeout() -> void:
+	coyote_jump = false
